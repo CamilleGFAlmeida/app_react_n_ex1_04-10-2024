74a81f89ec5e7bc0c845628e2477fb6637f5648f
Camille Guillen F. de Almeida <97997891+CamilleGFAlmeida@users.noreply.github.com>
2024-10-18 18:43:59 +0000

    alt 18-10

--- App.js
+++ App.js
@@ -1,45 +1,42 @@
 import { StatusBar } from 'react-native';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
   return (
     <>
-    <StatusBar barStyle="dark-content" backgroundColor="white"/> 
-    <SafeAreaView style={styles.conteudo}>
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Às Sextas usamos preto!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </SafeAreaView>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <SafeAreaView style={styles.conteudo} >
+        <View style={styles.container}>
+          <Text style={styles.titulo}>Hello World!</Text>          
+        </View>
+      </SafeAreaView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
   barra:{
-    StatusBar: 'light-content', 
+    StatusBar: 'light-content',
     backgroundColor: 'white',
-    height: 50, 
-  }
-  conteudo:{
-    flex: 1, 
-    backgroundColor: '#fff', 
-  }
+    height: 50,
+  },
+  conteudo: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20, 
     borderWidth: 5,
-    borderColor: 'blue', 
+    borderColor: 'red',
     margin: 10,
-    marginTop: 30,  
   },
   titulo:{
-    color: 'blue', 
-    fontSize: 24, 
-    fontWeight: 'bold', 
+    color: 'blue',
+    fontSize: 24,
+    fontWeight: 'bold',
   }
 });
